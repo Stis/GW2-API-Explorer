74be8f393af0bee0e021d18867b5b71d41348aee
Stis <Stis@users.noreply.github.com>
2018-07-31 15:16:36 +0200

    Less spaces

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
 //JSON syntax highlighting. Found this somewhere on stackoverflow
 function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
+  if (typeof json != 'string') {
+    json = JSON.stringify(json, undefined, 2);
+  }
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+    var cls = 'number';
+    if (/^"/.test(match)) {
+      if (/:$/.test(match)) {
+        cls = 'key';
+      } else {
+        cls = 'string';
+      }
+    } else if (/true|false/.test(match)) {
+        cls = 'boolean';
+    } else if (/null/.test(match)) {
+        cls = 'null';
     }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
+    return '<span class="' + cls + '">' + match + '</span>';
+  });
 }
 
 
 function getURL(endPoint, key, params) {
-    var url = "https://api.guildwars2.com"+endPoint+"?1";
-    key ? url += "&access_token="+key : "";
-    if (params) {
-        params.ids ? url += "&ids=" + params.ids : "";
-        params.lang ? url += "&lang=" + params.lang : "";
-        params.cid ? url = url.replace(":id", params.cid) : "";
-        params.cbo ? url = url.replace(":board", params.cbo) : "";
-        params.cre ? url = url.replace(":region", params.cre) : "";
-    }
-    return url;
+  var url = "https://api.guildwars2.com"+endPoint+"?1";
+  key ? url += "&access_token="+key : "";
+  if (params) {
+    params.ids ? url += "&ids=" + params.ids : "";
+    params.lang ? url += "&lang=" + params.lang : "";
+    params.cid ? url = url.replace(":id", params.cid) : "";
+    params.cbo ? url = url.replace(":board", params.cbo) : "";
+    params.cre ? url = url.replace(":region", params.cre) : "";
+  }
+  return url;
 }
 
 var guids = JSON.parse(localStorage.getItem("guids") || "[]");
 $.each(guids, function(i, key) {
-    $.getJSON(getURL("/v2/account", key), function(accData) {
-        $("#comptes").append("<label><input type=\"radio\" name=\"compte\" value=\""+key+"\">"+accData.name+"</label><br>");
-        $.getJSON(getURL("/v2/characters", key), function(result) {
-            var thediv = $("<persos/>").addClass("hidden");
-            $.each(result, function(i, perso) {
-                thediv.append("<label><input type=\"radio\" name=\"perso\" value=\""+perso+"\">"+perso+"</label><br>");
-            });
-            $("input[value=\""+key+"\"]").parent().next("br").after(thediv);
-        }).done(addEv);
-    });
+  $.getJSON(getURL("/v2/account", key), function(accData) {
+    $("#comptes").append("<label><input type=\"radio\" name=\"compte\" value=\""+key+"\">"+accData.name+"</label><br>");
+    $.getJSON(getURL("/v2/characters", key), function(result) {
+      var thediv = $("<persos/>").addClass("hidden");
+      $.each(result, function(i, perso) {
+        thediv.append("<label><input type=\"radio\" name=\"perso\" value=\""+perso+"\">"+perso+"</label><br>");
+      });
+      $("input[value=\""+key+"\"]").parent().next("br").after(thediv);
+    }).done(addEv);
+  });
 });
 
 $.getJSON("https://api.guildwars2.com/v2.json", function(data) {
-    $.each(data.routes, function(i, ep) {
-        var ald = ep.lang === true ? "l" : "";
-        ald += ep.auth === true ? " a" : "";
-        ald += ep.active === true ? "" : " d";
-        $("#eps").append($("<label/>", {class: ald, text: ep.path}).prepend($("<input/>", {type: "radio", name: "ep"})), $("<br/>"));
-    });
+  $.each(data.routes, function(i, ep) {
+    var ald = ep.lang === true ? "l" : "";
+    ald += ep.auth === true ? " a" : "";
+    ald += ep.active === true ? "" : " d";
+    $("#eps").append($("<label/>", {class: ald, text: ep.path}).prepend($("<input/>", {type: "radio", name: "ep"})), $("<br/>"));
+  });
 }).done(addEv);
 
 $(window).on("load", function() {
-    if (guids) {
-        $.each(guids, function() {
-            $("#keyList").append(this+"\n");
-        });
-        $("#keyList").html($("#keyList").val().replace(/\n$/, ""));
-    }
-    $("#saveKeys").click(function() {
-        localStorage.setItem("guids", JSON.stringify($("#keyList").val().split(/\n| |,/)));
+  if (guids) {
+    $.each(guids, function() {
+      $("#keyList").append(this+"\n");
     });
-    $("#delKeys").click(function() {
-        localStorage.removeItem("guids");
-        $("#keyList").empty();
-    });
-    $(".flag").click(function() {
-        $(this).siblings().addClass("d");
-        $(this).is(":not(.d)") ? $(this).addClass("d") : $(this).removeClass("d");
-        getData();
-    });
+    $("#keyList").html($("#keyList").val().replace(/\n$/, ""));
+  }
+  $("#saveKeys").click(function() {
+    localStorage.setItem("guids", JSON.stringify($("#keyList").val().split(/\n| |,/)));
+  });
+  $("#delKeys").click(function() {
+    localStorage.removeItem("guids");
+    $("#keyList").empty();
+  });
+  $(".flag").click(function() {
+    $(this).siblings().addClass("d");
+    $(this).is(":not(.d)") ? $(this).addClass("d") : $(this).removeClass("d");
+    getData();
+  });
 });
 
 function addEv() {
